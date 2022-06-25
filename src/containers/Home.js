5b08a55aa07d9daedc3ba01da95e34b86b3b7c0d
hamstersnore <hamstersnore@gmail.com>
2022-06-25 21:18:20 +0200

    compilation fix 2

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,3 @@
-import ScheduleDayColumn from '../components/schedule-day-column'
 function Home() {
     return (
         <div className="h-full w-2/3 flex-col items-end p-10 text-white text-6xl">
